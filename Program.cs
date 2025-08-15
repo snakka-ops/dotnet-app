88d81290f3e58e59e5ddcd938605edf7a7e90450
snakka-ops <sarathnakka39@gmail.com>
2025-08-15 18:28:37 +0530

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace DotNetApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from .NET!");
+        }
+    }
+}
